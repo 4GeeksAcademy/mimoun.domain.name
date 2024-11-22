c44473dc30c855d9ddd9d703dfedd373954ef1fd
Mimoun El Yazidi <87827134+min45@users.noreply.github.com>
2024-11-22 19:20:56 +0000

    domain name

--- src/app.js
+++ src/app.js
@@ -7,15 +7,15 @@ let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
 window.onload = function() {
-  let domainList = ""; // Create a variable to store all generated domain names
+  let domainList = "";
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        domainList += pronoun[i] + adj[j] + noun[k] + ".com<br>"; // Append each domain with a line break
+        domainList += pronoun[i] + adj[j] + noun[k] + ".com<br>";
       }
     }
   }
 
-  document.querySelector("#domain").innerHTML = domainList; // Add all domains to the innerHTML at once
+  document.querySelector("#domain").innerHTML = domainList;
 };
